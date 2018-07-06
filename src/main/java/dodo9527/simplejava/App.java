b71c92664f178e384103f75bf9c3d84406c09b7b
derder <derder9527@gmail.com>
2018-07-07 01:39:36 +0800

    first commit

---
+++ src/main/java/dodo9527/simplejava/App.java
@@ -0,0 +1,13 @@
+package dodo9527.simplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
